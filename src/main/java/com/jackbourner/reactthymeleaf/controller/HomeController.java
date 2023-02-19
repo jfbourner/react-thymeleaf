c1248623e6779c5b98dd444fd4daef2333da2ec5
jack <jack>
2023-02-19 00:22:47 +0000

    Fixed recaptcha sitekey being  hard coded  in home page

--- src/main/java/com/jackbourner/reactthymeleaf/controller/HomeController.java
+++ src/main/java/com/jackbourner/reactthymeleaf/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.jackbourner.reactthymeleaf.controller;
 
+import com.jackbourner.reactthymeleaf.config.CaptchaSettings;
 import com.jackbourner.reactthymeleaf.mail.ContactForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SuppressWarnings("unused")
 public class HomeController {
 
+    @Autowired
+    CaptchaSettings captchaSettings;
+
     @RequestMapping(value = "/")
     public String index(Model model) {
         model.addAttribute("contactForm", new ContactForm());
+        model.addAttribute("siteKey",captchaSettings.site());
         return "index";
     }
 }
